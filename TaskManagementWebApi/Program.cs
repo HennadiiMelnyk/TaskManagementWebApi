fa8f27a7e9fb514387dec4b3afb0a672d0d7680b
Hennadii Melnyk <h.melnyk@carma.taxi>
2025-06-04 02:23:30 +0200

    Add swagger, docker-compose.yml

--- TaskManagementWebApi/Program.cs
+++ TaskManagementWebApi/Program.cs
@@ -1,5 +1,39 @@
+using System.Text.Json.Serialization;
+using TaskManagementWebApi.Host.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddRepositories(builder.Configuration)
+    .AddServices()
+    .AddSwaggerWithApiVersioning();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Task Management API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.MigrateDatabase();
 
 app.MapGet("/", () => "Hello World!");
 
